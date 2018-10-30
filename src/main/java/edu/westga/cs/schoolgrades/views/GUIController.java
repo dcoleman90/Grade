be80768b9d8cb4a4ae8241524b060c9df64d2fc1
Drew Coleman <dcolem12@my.westga.edu>
2018-10-29 22:17:44 -0400

    The view now has three Vpanes which reflect the three list views - the quiz now successfully has a button which pops up an alert to allow the user to enter in a value for a new quiz score

--- src/main/java/edu/westga/cs/schoolgrades/views/GUIController.java
+++ src/main/java/edu/westga/cs/schoolgrades/views/GUIController.java
@@ -1,6 +1,11 @@
 package edu.westga.cs.schoolgrades.views;
 
 import javafx.scene.layout.GridPane;
+import javafx.util.Callback;
+
+import java.util.Optional;
+
+import edu.westga.cs.schoolgrades.model.Grade;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
@@ -11,10 +16,11 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonBar.ButtonData;
-
+import javafx.scene.control.TextInputDialog;
 
 public class GUIController extends GridPane {
 	@FXML
@@ -29,15 +35,65 @@ public class GUIController extends GridPane {
 	private ObservableList<Double> homework;
 	@FXML
 	private ObservableList<Double> exam;
-	
 
 	public GUIController() {
-		this.quiz =  FXCollections.observableArrayList();
-		this.homework =  FXCollections.observableArrayList();
-		this.exam =  FXCollections.observableArrayList();
-		
+		this.quiz = FXCollections.observableArrayList();
+		this.homework = FXCollections.observableArrayList();
+		this.exam = FXCollections.observableArrayList();
+
 		this.lvExam = new ListView<Double>();
 		this.lvHomework = new ListView<Double>();
 		this.lvQuiz = new ListView<Double>();
+		this.lvQuiz.setEditable(true);
+
+		this.lvQuiz.setItems(this.quiz);
+		this.lvExam.setItems(this.exam);
+		this.lvHomework.setItems(this.homework);
 	}
+
+	@FXML
+	private void addQuiz(double quizAdded) {
+		this.quiz.add(quizAdded);
+		this.lvQuiz.setItems(this.quiz);
+		this.lvQuiz.setEditable(true);
+	}
+
+	@FXML
+	private void addQuizButton() {
+		double quizAdded;
+
+		TextInputDialog addQuiz = new TextInputDialog("Quiz Grade in numeric form");
+		addQuiz.setTitle("Add Quiz result");
+		addQuiz.setHeaderText("Please insert a Quiz Grade result");
+		addQuiz.setContentText("Please enter your value here");
+		Optional<String> result = addQuiz.showAndWait();
+		if (result != null) {
+			quizAdded = this.getUserSelectedDouble(result.get());
+			if (quizAdded < 0) {
+				this.incorrectValueAlert();
+			} else {
+				this.addQuiz(quizAdded);
+			}
+		}
+	}
+
+	private double getUserSelectedDouble(String newStartingValue) {
+		double userEnteredValue = -5;
+		try {
+			userEnteredValue = Double.parseDouble(newStartingValue);
+		} catch (IllegalArgumentException iae) {
+			userEnteredValue = -5;
+		}
+		return userEnteredValue;
+	}
+	
+	private void incorrectValueAlert() {
+		Alert alert = new Alert(AlertType.WARNING);
+		alert.setTitle("Warning Incorrect Value");
+		alert.setHeaderText("All Added Grades must be in numeric form");
+		alert.setContentText("Values cannot be negitive numbers");
+
+		alert.showAndWait();
+	}
+
 }
